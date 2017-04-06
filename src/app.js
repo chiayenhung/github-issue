44277b81c796607d5db6fe82b533cf9cfc8b39d8
chiayenhung <hodalalala@gmail.com>
2017-04-05 21:38:13 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by chia-yenhung on 4/5/17.
+ */
+import React from "react";
+import ReactDom from "react-dom";
+
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  );
+};
+
+ReactDom.render(<Home/>, document.getElementById("app"));
